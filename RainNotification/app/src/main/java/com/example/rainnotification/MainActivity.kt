4d54bd40dc1502dc4e9cd6790f391db84b30c7ad
Daoyuan Yang <u6547564@anu.edu.au>
2019-06-27 00:18:07 +1000

    First commit

---
+++ RainNotification/app/src/main/java/com/example/rainnotification/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.rainnotification
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        titleMessage.text = "Here is the forecast for rain!"
+        button.setOnClickListener {
+            Resource.main();
+        }
+    }
+
+
+}
