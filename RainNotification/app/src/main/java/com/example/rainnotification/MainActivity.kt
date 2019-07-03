484c91239ca613146f25e460a48ef3cda9001481
Daoyuan Yang <u6547564@anu.edu.au>
2019-07-03 22:28:17 +1000

    Network connection works, but needs to do more on scrapping

--- RainNotification/app/src/main/java/com/example/rainnotification/MainActivity.kt
+++ RainNotification/app/src/main/java/com/example/rainnotification/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.rainnotification
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log.d
 import android.widget.EditText
+import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.JsonRequest
+import com.android.volley.toolbox.Volley
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import org.jsoup.Connection
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,10 +36,9 @@ class MainActivity : AppCompatActivity() {
             val DomIntent = Intent(this, DisplayMessageActivity::class.java).apply {
                 putExtra("MAINACTIVITY_MESSAGE", message)
             }
-            startActivity(DomIntent)            //            Resource.main();
-//            startActivity(Intent(this, Details::class.java))
-//            d("Main page text panel", "Hello, ${editText2.text}")
+            startActivity(DomIntent)
         }
+
 
         buttonToDetails.setOnClickListener {
             val intentDet = Intent(this, weather_details::class.java).apply {
@@ -39,8 +46,26 @@ class MainActivity : AppCompatActivity() {
             startActivity(intentDet)
         }
 
-
+        // Refresh data
+//        makeRequest()
+        refreshButton.setOnClickListener {
+            makeRequest()
+        }
     }
 
+    fun makeRequest(){
+        val url = "https://samples.openweathermap.org/data/2.5/forecast/daily?id=524901&appid=ee114fcf17bfeb308215ed3f430824f7"
+        val queue = Volley.newRequestQueue(this)
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                textView.text = "Response: %s".format(response.toString())
+            },
+            Response.ErrorListener { error ->
+                // TODO: Handle error
+                d("Error: ", "Error occurred in JsonObjectRequest!!!")
+            }
+        )
+        queue.add(jsonObjectRequest)
+    }
 
 }
