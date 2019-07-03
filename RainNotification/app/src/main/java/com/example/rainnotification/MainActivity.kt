c5a8fed9ead0f69345fd614e063ca1320f96b192
Daoyuan Yang <u6547564@anu.edu.au>
2019-07-03 23:34:35 +1000

    Search engine works now

--- RainNotification/app/src/main/java/com/example/rainnotification/MainActivity.kt
+++ RainNotification/app/src/main/java/com/example/rainnotification/MainActivity.kt
@@ -4,20 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log.d
 import android.widget.EditText
-import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.JsonRequest
 import com.android.volley.toolbox.Volley
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import org.jsoup.Connection
 
 class MainActivity : AppCompatActivity() {
+    val url_BASE = "https://api.openweathermap.org/data/2.5/weather?q="
+    var id = ""
+    val url_END = "&appid=ee114fcf17bfeb308215ed3f430824f7"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,14 +31,21 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        buttonForTest.setOnClickListener {
+        buttonSearch.setOnClickListener {
             val editText = findViewById<EditText>(R.id.editText)
-            val message = editText.text.toString()
-            val DomIntent = Intent(this, DisplayMessageActivity::class.java).apply {
-                putExtra("MAINACTIVITY_MESSAGE", message)
-            }
-            startActivity(DomIntent)
+            val city = editText.text.toString()
+            id = city //"524901"
+            textView.text = "Refreshing......"
+            makeRequest()
+
+//            val editText = findViewById<EditText>(R.id.editText)
+//            val message = editText.text.toString()
+//            val DomIntent = Intent(this, DisplayMessageActivity::class.java).apply {
+//                putExtra("MAINACTIVITY_MESSAGE", message)
+//            }
+//            startActivity(DomIntent)
         }
+
 
 
         buttonToDetails.setOnClickListener {
@@ -47,18 +55,22 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Refresh data
-//        makeRequest()
+        makeRequest()
         refreshButton.setOnClickListener {
+            textView.text = "Refreshing......"
             makeRequest()
         }
     }
 
     fun makeRequest(){
-        val url = "https://samples.openweathermap.org/data/2.5/forecast/daily?id=524901&appid=ee114fcf17bfeb308215ed3f430824f7"
+        val finalURL = url_BASE + id + url_END
+//        val url = "https://samples.openweathermap.org/data/2.5/forecast/daily?id=524901&appid=ee114fcf17bfeb308215ed3f430824f7"
+        d("Final URL: ", finalURL)
         val queue = Volley.newRequestQueue(this)
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, finalURL, null,
             Response.Listener { response ->
                 textView.text = "Response: %s".format(response.toString())
+                d("Weather info: ", "Response: %s".format(response.toString()))
             },
             Response.ErrorListener { error ->
                 // TODO: Handle error
@@ -66,6 +78,7 @@ class MainActivity : AppCompatActivity() {
             }
         )
         queue.add(jsonObjectRequest)
+
     }
 
 }
