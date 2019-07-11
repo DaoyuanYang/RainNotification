97d545f5cc8d06c55845f8fed70f056f1b7cf869
Daoyuan Yang <u6547564@anu.edu.au>
2019-07-11 10:59:55 +1000

    AlarmManager has some issue

--- RainNotification/app/src/main/java/com/example/rainnotification/MainActivity.kt
+++ RainNotification/app/src/main/java/com/example/rainnotification/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.rainnotification
 
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -7,14 +8,12 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.util.Log.d
 import android.widget.EditText
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
@@ -23,6 +22,7 @@ import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import org.json.JSONObject
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     val url_BASE = "https://api.openweathermap.org/data/2.5/weather?q="
@@ -31,6 +31,14 @@ class MainActivity : AppCompatActivity() {
 
     var jsonFmtElem = arrayOf<String>("coord", "weather", "base", "main"
                     , "visibility", "wind", "clouds", "timezone", "name", "id")
+
+    val CHANNEL_ID = "weatherNotification"
+
+    var CITY_LIST = arrayListOf<String>("Canberra", "Sydney")
+
+    var JSON_response = JSONObject()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,29 +50,35 @@ class MainActivity : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
 
+        id = "Canberra"
+        textView.text = "Refreshing......"
+        makeRequest()
 
         buttonSearch.setOnClickListener {
             val editText = findViewById<EditText>(R.id.editText)
             val city = editText.text.toString()
             id = city //"524901"
-            textView.text = "Refreshing......"
+            textView.text = "Searching......"
             makeRequest()
 
 //            val editText = findViewById<EditText>(R.id.editText)
 //            val message = editText.text.toString()
-//            val DomIntent = Intent(this, DisplayMessageActivity::class.java).apply {
+//            val passMsgIntent = Intent(this, DisplayMessageActivity::class.java).apply {
 //                putExtra("MAINACTIVITY_MESSAGE", message)
 //            }
-//            startActivity(DomIntent)
+//            startActivity(passMsgIntent)
         }
 
-
-
-        buttonToDetails.setOnClickListener {
-            val intentDet = Intent(this, weather_details::class.java).apply {
-            }
-            startActivity(intentDet)
+        /*
+        sendNotificationButton.setOnClickListener {
+            createNotificationChannel()
+            sendNotification()
+//            val detailsIntent = Intent(this, weather_details::class.java).apply {
+//            }
+//            startActivity(detailsIntent)
         }
+*/
+
 
         // Refresh data
         makeRequest()
@@ -72,7 +86,11 @@ class MainActivity : AppCompatActivity() {
             textView.text = "Refreshing......"
             makeRequest()
         }
+
+//        setTimer()
     }
+
+
 
     fun makeRequest(){
         val finalURL = url_BASE + id + url_END
@@ -82,6 +100,7 @@ class MainActivity : AppCompatActivity() {
         var jsonObjectRequest = JsonObjectRequest(Request.Method.GET, finalURL, null,
             Response.Listener { response ->
                 var unformattedJson = JsonFormatter(response)
+                JSON_response = response
                 textView.text = "Response: %s".format(formatJSON(unformattedJson))
                 d("Weather info: ", "Response: %s\n".format(response.toString()))
             },
@@ -107,28 +126,9 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
+/*
     // Notification
     //
-// Create an explicit intent for an Activity in your app
-    val myIntent = Intent(this, AlertDetails::class.java).apply {
-        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-    }
-
-    val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
-    val CHANNEL_ID = ""
-    val NOTIFICATION_ID = 1
-
-    val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-        .setSmallIcon(R.drawable.notification_icon_background)
-        .setContentTitle("My notification")
-        .setContentText("Hello World!")
-        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-        // Set the intent that will fire when the user taps the notification
-        .setContentIntent(pendingIntent)
-        .setAutoCancel(true)
-
-
 
     private fun createNotificationChannel() {
         // Create the NotificationChannel, but only on API 26+ because
@@ -147,10 +147,50 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    with(NotificationManagerCompat.from(this)) {
-        // notificationId is a unique int for each notification that you must define
-        (NOTIFICATION_ID, builder.build())
-    }
 
+//    NotificationManagerCompat.notify()
+
+    private fun sendNotification(){
+        // Notification
+        // Create an explicit intent for an Activity in your app
+        val myIntent = Intent(this, AlertDetails::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+        val NOTIFICATION_ID = 1
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.notification_icon_background)
+            .setContentTitle("Weather Notification")
+            .setContentText(JSON_response.get("main").toString())
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        createNotificationChannel()
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(NOTIFICATION_ID, builder.build())
+        }
+    }
+*/
+    fun setTimer(){
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, 23)
+        calendar.set(Calendar.MINUTE, 2)
+        calendar.set(Calendar.SECOND, 0)
+
+        val intentFromTimer = Intent(applicationContext, OnReceive::class.java).apply {
+            putExtra("MAIN_WEATHER_INFO", JSON_response.get("main").toString())
+        }
+
+        val pendingIntent = PendingIntent.getBroadcast(applicationContext, 100, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val alarmManager: AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent)
+
+        startActivity(intentFromTimer)
+    }
 
 }
